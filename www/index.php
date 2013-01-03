e4cb4cb0017c4e5daf08040212fd8812eb7a547f
jedick <jedick@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-01-03 04:48:59 +0000

    www: customize index.php; show random image
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/chnosz@37 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -1,3 +1,4 @@
+<!-- CHNOSZ project web page for R-Forge -->
 
 <!-- This is the project specific website template -->
 <!-- It can be changed as liked or replaced by other content -->
@@ -18,7 +19,7 @@ echo '<?xml version="1.0" encoding="UTF-8"?>';
   <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	<title><?php echo $group_name; ?></title>
-	<link href="<?php echo $themeroot; ?>styles/estilo1.css" rel="stylesheet" type="text/css" />
+	<link href="theme.css" rel="stylesheet" type="text/css" />
   </head>
 
 <body>
@@ -26,7 +27,7 @@ echo '<?xml version="1.0" encoding="UTF-8"?>';
 <!-- R-Forge Logo -->
 <table border="0" width="100%" cellspacing="0" cellpadding="0">
 <tr><td>
-<a href="http://r-forge.r-project.org/"><img src="http://<?php echo $themeroot; ?>/images/logo.png" border="0" alt="R-Forge Logo" /> </a> </td> </tr>
+<a href="http://r-forge.r-project.org/"><img src="logo.png" border="0" alt="R-Forge Logo" /> </a> </td> </tr>
 </table>
 
 
@@ -43,9 +44,55 @@ echo $contents; } ?>
 
 <!-- end of project description -->
 
-<p> No content added. </p>
+<p> The <strong>project summary page</strong> you can find <a href="http://<?php echo $domain; ?>/projects/<?php echo $group_name; ?>/"><strong>here</strong></a> on R-Forge. (Development site, source code management)</p>
 
-<p> The <strong>project summary page</strong> you can find <a href="http://<?php echo $domain; ?>/projects/<?php echo $group_name; ?>/"><strong>here</strong></a>. </p>
+<p> Currently (Jan. 2013) the main website for the project is located at <a href="http://www.chnosz.net"><strong>chnosz.net</strong></a>. (Downloads, help pages, output from examples and vignettes) </p>
+
+<p> As a teaser, here's a random image from the examples of CHNOSZ 0.9-9, released on CRAN on 2013-01-01. Click <a href="images/"><strong>here</strong></a> for a directory listing of all 54 images, or <a href="http://www.chnosz.net/examples/examples.html"><strong>here</strong></a> for the images in context of the code running the examples. </p>
+
+<!-- random image code -->
+
+<?php
+function getImagesFromDir($path) {
+    $images = array();
+    if ( $img_dir = @opendir($path) ) {
+        while ( false !== ($img_file = readdir($img_dir)) ) {
+            // checks for gif, jpg, png
+            if ( preg_match("/(\.gif|\.jpg|\.png)$/", $img_file) ) {
+                $images[] = $img_file;
+            }
+        }
+        closedir($img_dir);
+    }
+    return $images;
+}
+
+function getRandomFromArray($ar) {
+    mt_srand( (double)microtime() * 1000000 ); // php 4.2+ not needed
+    $num = array_rand($ar);
+    return $ar[$num];
+}
+
+/////////////////////////////////////////////////////////////////////
+// This is the only portion of the code you may need to change.
+// Indicate the location of your images 
+
+$root = '';
+// use if specifying path from root
+//$root = $_SERVER['DOCUMENT_ROOT'];
+
+$path = 'images/';
+
+// End of user modified section 
+/////////////////////////////////////////////////////////////////////
+
+// Obtain list of images from directory 
+$imgList = getImagesFromDir($root . $path);
+$img = getRandomFromArray($imgList);
+?> 
+
+<p>Image filename: <?php echo $img ?></p>
+<img src="<?php echo $path . $img ?>" alt="" />
 
 </body>
 </html>
